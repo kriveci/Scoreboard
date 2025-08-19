ae33d112a3ef7ca07a8949ca9e93b0ecc39664fe
kriveci <igor.krivec@gmail.com>
2025-08-19 21:44:18 +0200

    Add failing test for starting match

--- Scoreboard.Test/ScoreboardTests.cs
+++ Scoreboard.Test/ScoreboardTests.cs
@@ -3,9 +3,19 @@ namespace Scoreboard.Test
     public class ScoreboardTests
     {
         [Fact]
-        public void Test1()
+        public void StartMatch_AddsMatchWithInitialScore()
         {
+            var scoreBoard = new Scoreboard();
 
+            scoreBoard.StartMatch("Slovenia", "Croatia");
+
+            var summary = scoreBoard.GetSummary();
+
+            Assert.Single(summary);
+            Assert.Equal(0, summary[0].HomeScore);
+            Assert.Equal(0, summary[0].AwayScore);
+            Assert.Equal("Slovenia", summary[0].HomeTeam);
+            Assert.Equal("Croatia", summary[0].AwayTeam);
         }
     }
 }
