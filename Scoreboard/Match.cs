d26c0d6371e49f7eb1cfd40288ce7fdd59371c61
kriveci <igor.krivec@gmail.com>
2025-08-19 21:55:09 +0200

    Implement StartMatch

---
+++ Scoreboard/Match.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoreboard
+{
+    public class Match
+    {
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public Match(string homeTeam, string awayTeam)
+        {
+            HomeTeam = homeTeam;
+            AwayTeam = awayTeam;
+            StartTime = DateTime.UtcNow;
+        }
+    }
+}
--- Scoreboard/Scoreboard.cs
+++ Scoreboard/Scoreboard.cs
@@ -1,7 +1,19 @@
-﻿namespace Scoreboard
+﻿using System.Text.RegularExpressions;
+
+namespace Scoreboard
 {
     public class Scoreboard
     {
+        private List<Match> _matches = new();
 
+        public void StartMatch(string homeTeam, string awayTeam)
+        {
+            _matches.Add(new Match(homeTeam, awayTeam));
+        }
+
+        public List<Match> GetSummary()
+        {
+            return _matches;
+        }
     }
 }
